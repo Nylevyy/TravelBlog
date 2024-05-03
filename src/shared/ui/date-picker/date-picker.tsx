b3cafafea101b22415c234e8dac376b7959b8650
Georgii Zaikin <georgii_zaikin@epam.com>
2024-05-03 23:33:45 +0400

    feat(#TS): moved date picker to TS

---
+++ src/shared/ui/date-picker/date-picker.tsx
@@ -0,0 +1,44 @@
+import classNames from 'classnames/bind';
+import DateTimePicker, { registerLocale } from 'react-datepicker';
+import { ru } from 'date-fns/locale';
+import 'react-datepicker/dist/react-datepicker.css';
+import * as styles from './date-picker.scss';
+
+registerLocale('ru', ru);
+
+const ccn = classNames.bind(styles);
+
+type Props = {
+  notValidated: boolean;
+  value: Date;
+  onChange: (value: Date) => void;
+  id?: string;
+};
+
+const DatePicker = ({ notValidated, value, onChange, id = 'date' }: Props) => {
+  return (
+    <div className={styles.datePicker}>
+      <DateTimePicker
+        className={ccn('input', {
+          invalid: notValidated,
+        })}
+        calendarClassName={styles.calendar}
+        onChange={onChange}
+        onSelect={onChange}
+        selected={value}
+        id={id}
+        showTimeInput
+        locale="ru"
+        timeFormat="HH:mm"
+        timeCaption="time"
+        dateFormat="d MMMM, yyyy HH:mm"
+        popperClassName={styles.popper}
+      />
+      <label className={styles.label} htmlFor={id}>
+        Время
+      </label>
+    </div>
+  );
+};
+
+export default DatePicker;
